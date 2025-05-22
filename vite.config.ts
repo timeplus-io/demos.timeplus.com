251dee1654453b38d2ec60532cee570ce6249e86
Jove Zhong <jove@timeplus.io>
2025-05-22 12:26:59 -0700

    fix a image link (no /),try to support both GH and Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // Set base path dynamically based on environment
-const isProduction = process.env.NODE_ENV === "production";
-const basePath = isProduction ? "/demos.timeplus.com/" : "";
+const isGithubPage = process.env.GH_PAGE === "true";
+const basePath = isGithubPage ? "/demos.timeplus.com/" : "";
 
 export default defineConfig({
   base: basePath,
